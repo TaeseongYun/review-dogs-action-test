9a0b5e68cfe3fb575e863367b81c026f4af85a43
taeseongyun <yts0646@gmail.com>
2024-05-20 08:19:56 +0900

    [ Fix ] : 수정

--- detekt-own-rules/src/main/java/com/example/detekt/ServiceRule.kt
+++ detekt-own-rules/src/main/java/com/example/detekt/ServiceRule.kt
@@ -7,6 +7,7 @@ import io.gitlab.arturbosch.detekt.api.Entity
 import io.gitlab.arturbosch.detekt.api.Issue
 import io.gitlab.arturbosch.detekt.api.Rule
 import io.gitlab.arturbosch.detekt.api.Severity
+import org.jetbrains.kotlin.psi.KtClass
 import org.jetbrains.kotlin.psi.KtFile
 
 class ServiceRule(config: Config) : Rule(config) {
@@ -19,14 +20,17 @@ class ServiceRule(config: Config) : Rule(config) {
 
   override fun visitKtFile(file: KtFile) {
     super.visitKtFile(file)
-    if (file.name.contains("Service")) {
-      report(
-        CodeSmell(
-          issue,
-          Entity.from(file),
-          message = "debug 와 release 둘 다 변경 적용하셨나요 ? 적용하였다면 이모지를 남겨주세요."
+    file.declarations.forEach { declaration ->
+      if (declaration is KtClass && declaration.isInterface() && declaration.name?.contains("Service") == true) {
+        report(
+          CodeSmell(
+            issue,
+            Entity.from(declaration),
+            message = "debug 와 release 둘 다 변경 적용하셨나요? 적용하였다면 이모지를 남겨주세요."
+          )
         )
-      )
+      }
     }
   }
 }
+
